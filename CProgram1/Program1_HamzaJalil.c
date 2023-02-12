4892eac418b35f41c747621688e21de6b8ae4e9d
hmj2000 <123510127+hmj2000@users.noreply.github.com>
2023-02-12 16:48:21 -0600

    Update Program1_HamzaJalil.c

--- CProgram1/Program1_HamzaJalil.c
+++ CProgram1/Program1_HamzaJalil.c
@@ -3,9 +3,9 @@
 
 #define MONTHS 12
 
-float*  read_data_from(char *file-name)// Reads in file
+float*  read_data_from(char *file-name) // Reads in file
     {
-        float *month_sales_data = (float*)malloc(sizeof(float) * months);
+        float *month_sales_data = (float*)malloc(sizeof(float) *months);
         FILE *fp = fopen(file_bname, "r*");
         for(int i = 0; i < MONTHS; ++i)
             {
@@ -14,33 +14,21 @@ float*  read_data_from(char *file-name)// Reads in file
         return month_sales_data;
     }
 
-void print_data(const char *months, const float *sales)//Prints Month Sales Data
+void print_data(const char *months, const float *sales) //Prints Month Sales Data
     {
         printf("Month Sales\n");
         for(int i = 0; i < MONTHS; ++i)
             {
-                printf("%30s %10f\n", months[i], sales[i];)
+                printf("%30s %10f\n", months[i], sales[i]);
             }
     }
 
-void sales_summary(const char *months, const float*sales)//Sales Summary
+float* return_max_sales(const char *months, const float *sales)
     {
-        double min_sales;// Type double variable for minimum sales amount
-        char min_sales_month;// Type char varaibale for month of minimum sales amount
-        double max_sales;// Type double variable for maximum sales amount
-        char max_sales_month;// Type char varaibale for month of maximum sales amount
-        double avg_sales;// Type double variable for average sales amount
-    
-        return min_sales, min_sales_month, max_sales, max_sales_month, avg_sales
-        
-    }
-
-float* return_max_sales(const char *months, const float*sales)
-    {
-        double *max = 0;
+        float *max = 0;
         char *month_max;
+    
         for(int i = 0; i < MONTHS; ++i)
-            
             {
                 if (sales[i] > max)
                 {
@@ -53,12 +41,12 @@ float* return_max_sales(const char *months, const float*sales)
     }
 
 
-float* return_min_sales(const char *months, const float*sales, float*max)
+float* return_min_sales(const char *months, const float *sales, float *max)
     {
-        double *min = max;
+        float *min = max;
         char *month_min;
+    
         for(int i = 0; i < MONTHS; ++i)
-            
             {
                 if (sales[i] < min)
                 {
@@ -70,29 +58,73 @@ float* return_min_sales(const char *months, const float*sales, float*max)
         return min, month_min
     }
 
-float* return_avg_sales(const float*sales)
+float* return_avg_sales(const float *sales)
     {
-        double *avg = 0;
+        float *avg = 0;
+    
         for(int i = 0; i < MONTHS; ++i)
             {
                 avg = avg + sales[i];
             }
+    
         avg = avg / MONTHS;
         return avg
     }
 
-void six_month_moving_average_report()//Six Month Moving Average Report
+void sales_summary(const char *months, const float *sales) //Sales Summary
     {
-        // Type double variable for sales amount for Janurary to June
-        // Type double variable for sales amount for February to July
-        // Type double variable for sales amount for March to August
-        // Type double variable for sales amount for April to September
-        // Type double variable for sales amount for May to October
-        // Type double variable for sales amount for June to November
-        // Type double variable for sales amount for July to December
+        float max_sales; // Type float variable for maximum sales amount
+        char max_sales_month; // Type char varaibale for month of maximum sales amount
+        max_sales, max_sales_month = return_max_sales(months, sales);
+        float min_sales; // Type float variable for minimum sales amount
+        char min_sales_month; // Type char varaibale for month of minimum sales amount
+        min_sales, min_sales_month = return_min_sales(months, sales, &max_sales)
+        float avg_sales; // Type float variable for average sales amount
+        avg_sales = return_avg_sales(sales)
+    
+        printf("Minimum Sales: %s    $%s \n", min_sales, min_sales_month); 
+        printf("Maximum Sales: %s    $%s \n", max_sales, max_sales_month); 
+        printf("Average Sales: %s \n", avg_sales);
+        
     }
 
-void sales_report()//Sales Report (Highest to Lowest)
+void six_month_moving_average_report(const float *sales) //Six Month Moving Average Report
+    {
+        float moving_average_1; // Type float variable for sales amount for Janurary to June
+        moving_average_1 = six_month_moving_average (1, 6, sales);
+        float moving_average_2; // Type float variable for sales amount for February to July
+        moving_average_2 = six_month_moving_average (2, 7, sales);
+        float moving_average_3; // Type float variable for sales amount for March to August
+        moving_average_3 = six_month_moving_average (3, 8, sales);
+        float moving_average_4; // Type float variable for sales amount for April to September
+        moving_average_4 = six_month_moving_average (4, 9, sales);
+        float moving_average_5; // Type float variable for sales amount for May to October
+        moving_average_5 = six_month_moving_average (5, 10, sales);
+        float moving_average_6; // Type float variable for sales amount for June to November
+        moving_average_6 = six_month_moving_average (6, 11, sales);
+        float moving_average_7; // Type float variable for sales amount for July to December
+        moving_average_7 = six_month_moving_average (7, 12, sales);
+    
+        printf(" Janurary to June:%s \n", moving_average_1);
+        printf(" February to July :%s \n", moving_average_2);
+        printf(" March to August :%s \n", moving_average_3);
+        printf(" April to September :%s \n", moving_average_4);
+        printf(" May to October :%s \n", moving_average_5);
+        printf(" June to November :%s \n", moving_average_6);
+        printf(" July to December :%s \n", moving_average_7);
+    }
+
+float six_month_moving_average (int start_month, int end_month, const float *sales)
+    {
+        float avg = 0;
+        for(int i = start_month - 1; i < end_month; ++i)
+            {
+                avg = avg + sales[i];
+            }
+        return avg
+    }
+
+void sales_report() //Sales Report (Highest to Lowest)
     {
         // Temp array to order sales based on amount from highest to lowest
         // Temp array to order months based on amount from highest to lowest
@@ -101,7 +133,7 @@ void sales_report()//Sales Report (Highest to Lowest)
 
 int main()
     {
-        float* sales = read_data_from("Sales.txt");
+        float *sales = read_data_from("Sales.txt");
         conts char *months[] = {"January", "February", "March" , "April" , "May" , "June" , "July" , "August" , "September" , "October" , "November" , "December"};
         print_data(months, sales);
         return 0;
