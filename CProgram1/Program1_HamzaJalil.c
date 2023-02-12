4ce2dc52a64a8e8575a50e3855012c7d39520ce3
Hamza Jalil <hmj.ks2000@gmail.com>
2023-02-12 01:03:00 -0600

    Added Comments

--- CProgram1/Program1_HamzaJalil.c
+++ CProgram1/Program1_HamzaJalil.c
@@ -3,7 +3,7 @@
 
 #define MONTHS 12
 
-float*  read_data_from(char *file-name)
+float*  read_data_from(char *file-name)// Reads in file
     {
         float* month_sales_data = (float*)malloc(sizeof(float) * months);
         FILE *fp = fopen(file_bname, "r*");
@@ -14,13 +14,38 @@ float*  read_data_from(char *file-name)
         return month_sales_data;
     }
 
-void print_data(const char *months, const float*sales)
+void print_data(const char *months, const float*sales)//Prints Month Sales Data
     {
         printf("Month Sales\n");
         for(int i = 0; i < MONTHS; ++i)
             {
                 printf("%30s %10f\n", months[i], sales[i];)
             }
+    }
+
+void sales_summary()//Sales Summary
+    {
+        // Type double variable for minimum sales amount
+        // Type double variable for maximum sales amount
+        // Type double variable for average sales amount 
+    }
+
+void six_month_moving_average_report()//Six Month Moving Average Report
+    {
+        // Type double variable for sales amount for Janurary to June
+        // Type double variable for sales amount for February to July
+        // Type double variable for sales amount for March to August
+        // Type double variable for sales amount for April to September
+        // Type double variable for sales amount for May to October
+        // Type double variable for sales amount for June to November
+        // Type double variable for sales amount for July to December
+    }
+
+void sales_report()//Sales Report (Highest to Lowest)
+    {
+        // Temp array to order sales based on amount from highest to lowest
+        // Temp array to order months based on amount from highest to lowest
+        // Loop to compare amounts and order them based on amount (highest to lowest) and have a correlating month array
     }
 
 int main()
